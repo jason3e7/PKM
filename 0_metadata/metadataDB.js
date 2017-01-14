905586871c9ab8f7e5ab3511347e9a83f4d7a87c
jason3e7 <jason3e7@gmail.com>
2017-01-15 01:15:45 +0800

    add metadataDB init

---
+++ 0_metadata/metadataDB.js
@@ -0,0 +1,15 @@
+var metadata = {
+}
+
+/*
+example
+{
+  "title" : {
+    "crtime" : "201701142332",
+    "mtime" : "201701142333",
+    "value" : "0 ~ 99",
+    "tags" : ["tag"],
+    "location" : { "/path/filename" : "self or lineRange"}
+  }
+}
+*/
